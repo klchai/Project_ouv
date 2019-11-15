76f542af9c84e44ff755338141ece00c94905ba9
XIANQw <qiwei-xian@qq.com>
2019-11-15 22:56:18 +0100

    pair

--- src/abr.ml
+++ src/abr.ml
@@ -35,7 +35,7 @@ let gen_permutation n =
 ;;
 
 (* Question 1.3 *)
-let rec ajoute arbre value =
+let rec ajoute (arbre : abr) value =
     match arbre with
     | Empty -> Node(value, Empty, Empty)
     | Node(v, left, right) ->
@@ -73,24 +73,83 @@ let printArbre arbre =
     Printf.printf "\n";
 ;;
 let printList l =
-    Printf.printf "list : \n";
-    let rec bfs arr = 
+    Printf.printf "list : ";
+    let rec helper arr = 
         match arr with
-        | [] -> Printf.printf "";
+        | [] -> Printf.printf "#";
         | x::rest -> 
             Printf.printf "%d " x;
-            bfs rest;
-    in bfs l;
+            helper rest;
+    in helper l;
     Printf.printf "\n";
 ;;
 
 (* Question 2.4 *)
+let rec abr_mot (a: abr) =
+    match a with
+    | Empty -> ""
+    | Node(v, l, r)-> "(" ^ (abr_mot l) ^ ")" ^ (abr_mot r)
+;;
 
+let isomorphe (a: abr) (b: abr) = 
+    let am = abr_mot a in
+    let bm = abr_mot b in
+    Printf.printf "a: %s\n" am;
+    Printf.printf "b: %s\n" bm;
+    am == bm
+;;
+
+type pair = None | Nodep of string * (int list)
+let print_pair p = 
+    match p with
+    | None -> Printf.printf "" ;
+    | Nodep(s, v_list)-> Printf.printf "%s :" s; printList v_list;
+;;
+
+let rec print_pair_list p_List =
+    match p_List with
+    |[] -> Printf.printf "" ;
+    |a::rest -> print_pair a; 
+                print_pair_list rest;
+;;
+
+let modify_list list v =
+    match !list with
+    |[] -> list:=  [v]
+    |a::rest -> list:= v::a::rest
+;;
+
+let rec ajoute_mot_value mot valeur list =
+    match !list with
+    |[] -> list := [Nodep(mot, [valeur])]; Printf.printf "(%s: %d)\n" mot valeur ;
+    |None::rest -> ajoute_mot_value mot valeur (ref rest)
+    |Nodep(s, v_list)::rest ->
+        Printf.printf "(%s: %d) Nodep %s" mot valeur s; printList v_list;
+        if s == mot then modify_list (ref v_list) valeur
+        else ajoute_mot_value mot valeur (ref rest)
+;;
+
+
+let change_Arbre_to_pairList abr =
+    let list = [] in
+    let rec helper abr list =
+    match abr with
+    |Empty -> "";
+    |Node(v, l, r)->
+        let mot = "(" ^ (helper l list) ^ ")" ^ (helper r list) in
+        ajoute_mot_value mot v list;
+        mot
+    in (helper abr (ref list)); 
+    list
+;;
 
 
 (* ------- main ------- *)
-let arr = gen_permutation 10
+(* let arr = gen_permutation 10 
 let () = printList arr
-let res = ajouteList Empty arr 
-let () = printArbre res
-let () = Printf.printf "%b\n" (search res 5);
+let b = ajouteList Empty arr *)
+let a_list = [4; 2; 1; 3; 8; 6; 5; 7; 9]
+let tmp = ref [9; 11]
+let a = ajouteList Empty a_list
+let p_list = change_Arbre_to_pairList a
+let () = print_pair_list p_list
